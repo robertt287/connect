7cbf9b3c40066616a9bc7c2d530f5b9b2a9cbef9
Dragomirescu Ionut <ionutdragomirescu6@gmail.com>
2024-01-09 19:52:19 +0200

    first commit

---
+++ src/test/java/com/example/conenct/ConenctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.conenct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConenctApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
